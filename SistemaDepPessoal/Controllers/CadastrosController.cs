01259d93d0e2bb97fba8f620a9cb2c34e832025b
David Machado <david.machado20@hotmail.com>
2023-01-25 18:01:36 -0300

    Adicionar arquivos de projeto.

---
+++ SistemaDepPessoal/Controllers/CadastrosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaDepPessoal.Controllers
+{
+    public class CadastrosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SistemaDepPessoal/Controllers/CalculadoraController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaDepPessoal.Controllers
+{
+    public class CalculadoraController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
